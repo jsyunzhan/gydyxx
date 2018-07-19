faa0ac19cf6f47ae0ff68fba130b92ec0ed20620
Mark <chen@qq.com>
2018-07-19 15:23:39 +0800

    文明创建

--- src/main/java/domain/home/entity/NewsEntity.java
+++ src/main/java/domain/home/entity/NewsEntity.java
@@ -4,12 +4,6 @@ import domain.shiro.entity.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-
-import static com.google.common.base.Strings.isNullOrEmpty;
-import static java.util.Arrays.copyOf;
-
 @Getter
 @Setter
 public class NewsEntity extends AbstractEntity{
@@ -17,38 +11,30 @@ public class NewsEntity extends AbstractEntity{
     //新闻标题
     private String newsTitle;
 
-    private byte[] newsDetails;
+    private byte[] newsDetailsByte;
 
     //新闻内容
-    private String newsDetailsStr;
+    private String newsDetails;
 
     //图片地址
     private String picturePath;
 
-    public byte[] getNewsDetails() {
-        return newsDetails == null ? new byte[0] : copyOf(newsDetails, newsDetails.length);
-    }
+//    public byte[] getNewsDetails() {
+//        return newsDetails == null ? new byte[0] : copyOf(newsDetails, newsDetails.length);
+//    }
+//
+//    public void setNewsDetails(byte[] newsDetails) {
+//        this.newsDetails = newsDetails == null ? new byte[0] : copyOf(newsDetails, newsDetails.length);
+//    }
+//
+//    public String getNewsDetailsStr() throws UnsupportedEncodingException {
+//        return newsDetails == null ? "" : new String(newsDetails, "UTF-8");
+//    }
+//
+//    public void setNewsDetailsStr(String newsDetailsStr) throws UnsupportedEncodingException {
+//        this.newsDetails = isNullOrEmpty(newsDetailsStr) ? new byte[0] : newsDetailsStr.getBytes("UTF-8");
+//    }
 
-    public void setNewsDetails(byte[] newsDetails) {
-        this.newsDetails = newsDetails == null ? new byte[0] : copyOf(newsDetails, newsDetails.length);
-    }
-
-    public String getNewsDetailsStr() throws UnsupportedEncodingException {
-        return newsDetails == null ? "" : new String(newsDetails, "UTF-8");
-    }
-
-    public void setNewsDetailsStr(String newsDetailsStr) throws UnsupportedEncodingException {
-        this.newsDetails = isNullOrEmpty(newsDetailsStr) ? new byte[0] : newsDetailsStr.getBytes("UTF-8");
-    }
 
 
-    @Override
-    public String toString() {
-        return "NewsEntity{" +
-                "newsTitle='" + newsTitle + '\'' +
-                ", newsDetails=" + Arrays.toString(newsDetails) +
-                ", newsDetailsStr='" + newsDetailsStr + '\'' +
-                ", picturePath='" + picturePath + '\'' +
-                '}';
-    }
 }
