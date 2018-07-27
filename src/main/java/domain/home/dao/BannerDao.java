c0526a8b0d674f90509730597d52c3d3105e7ba9
e <yourmail@youremail.com.cn>
2018-07-27 17:06:07 +0800

    学校概况

--- src/main/java/domain/home/dao/BannerDao.java
+++ src/main/java/domain/home/dao/BannerDao.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 public interface BannerDao {
+
     Integer bannerCount(BannerEntity bannerEntity);
 
     List<BannerEntity> bannerList(BannerEntity bannerEntity);
