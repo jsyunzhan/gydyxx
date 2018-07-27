ac0636ff477c0a5f5d9447d3067dd10ecaacfa74
linjian <707443496@qq.com>
2018-07-27 17:50:06 +0800

    1

--- src/main/webapp/WEB-INF/pages/wages/wagesquerymanagementpanel.jsp
+++ src/main/webapp/WEB-INF/pages/wages/wagesquerymanagementpanel.jsp
@@ -11,6 +11,40 @@
 <html>
 <head>
     <title>Title</title>
+    <style>
+        .wrapper{
+            width: 100%;
+            overflow-x: auto;
+            height: 100%;
+        }
+        .pay{
+            white-space: nowrap;
+            font-size: 0;
+            margin: 10px;
+        }
+        .pay .payNum{
+            width: 120px;
+            line-height: 40px;
+            font-size: 20px;
+            text-align: center;
+            border-bottom: 1px solid #000;
+            font-size: 14px;
+            box-sizing: border-box;
+            white-space: normal;
+            word-wrap: break-word;
+            word-break: break-all;
+            overflow: hidden;
+            display: inline-block;
+        }
+        .payNum p{
+            border: 1px solid #000;
+            border-bottom: 0;
+            margin: 0;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+    </style>
     <jsp:include flush="true" page="/WEB-INF/pages/commons/servercommonjs.jsp"/>
 
     <script type="text/javascript"
@@ -18,7 +52,8 @@
 </head>
 <body class="easyui-layout">
 
-
+    <div class="wrapper">
+    </div>
 
 </body>
 </html>
--- src/main/webapp/static/js/wages/wagesquerymanagementpanel.js
+++ src/main/webapp/static/js/wages/wagesquerymanagementpanel.js
@@ -1,11 +1,32 @@
 $(function () {
+    // 日期格式转换
+    function timestampToTime01(timestamp){
+        var date = new Date(timestamp);
+        Y = date.getFullYear() + '-';
+        M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+        D = date.getDate() + ' ';
+        return Y+M+D;
+    }
 
     $.ajax({
         url: path + '/wages/wagesquerymanpage/list',
         type: 'get',
         dataTye: 'json',
         success: function (data) {
-            console.log(data)
+            console.log(data);
+            var _html = "";
+            for(var i=0;i<data.length;i++){
+                _html += '<div class="pay"><div class="payNum"><p title="姓名">姓名</p><p>'+data[i].accountName+'</p></div>';
+                _html += '<div class="payNum"><p title="发放时间">发放时间</p><p>'+timestampToTime01(data[i].wagesData)+'</p></div>';
+                _html += '<div class="payNum"><p title="工资名称">工资名称</p><p>'+data[i].wagesName+'</p></div>';
+                var data01 = data[i].wagesdetails.split(";");
+                for(var j=0;j<data01.length-1;j++){
+                    var data02 = data01[j].split(":");
+                    _html += '<div class="payNum"><p title="'+data02[0]+'">'+data02[0]+'</p><p>'+data02[1]+'</p></div>';
+                }
+                _html += '</div>';
+            }
+            $(".wrapper").append(_html);
         }
     });
 
