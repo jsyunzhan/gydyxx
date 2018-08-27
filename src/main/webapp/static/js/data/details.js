556b58c4c143b9f3b63f2645982688f416a4fe78
linjian <707443496@qq.com>
2018-08-27 17:55:38 +0800

    1

--- src/main/webapp/static/js/data/details.js
+++ src/main/webapp/static/js/data/details.js
@@ -8,15 +8,16 @@ $(function () {
             url:path + "/home/picture/show",
             type:"POST",contentType: "application/json",data:JSON.stringify(data),async:false,
             success:function(event){
-                for (var i=0;i<event.length;i++){
-                    _html += '<img src="data:image/gif;base64,'+event[i]+'"  class="imgDetial">';
-                }
+                var dd = details.replace(/<p>/g,"<div class='newsCon_font'>");
+                dd = dd.replace(/<\/p>/g,"</div>");
+                dd = dd.replace(/{\d+}/g,function (str) {
+                    var str = Number(str.match(/\d+/g));
+                    str--;
+                    return '<img src="data:image/gif;base64,'+event[str]+'" class="imgDetial">';
+                });
+                _html += dd;
+                $(".newsCon").append(_html);
             }
         })
     }
-    var dd = details.replace(/<p>/g,"<div class='newsCon_font'>");
-    dd = dd.replace(/<\/p>/g,"</div>");
-    _html += dd;
-    $(".newsCon").append(_html);
-
 })
